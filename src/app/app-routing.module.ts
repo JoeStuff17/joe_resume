abf14027b78a8b9b715a3afeb565a06a4abd1f4f
Jothiraj <jothiraj@cloudtechdigital.co.in>
2024-05-19 17:56:08 +0530

    chat-bot changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,6 @@ const routes: Routes = [
   {
     path:'',
     pathMatch:'full',
-    redirectTo:'/home'
-  },
-  {    
-    path:'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   },
 ];
