c8d41fe7cfc51b3e1ec2b147eb6412637a4b9412
Jothiraj <jothiraj@cloudtechdigital.co.in>
2024-06-10 15:57:46 +0530

    robots file added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { title } from 'process';
 
 const routes: Routes = [
   {
     path:'',
     pathMatch:'full',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    data:{
+      title: "Joe's::Portfolio",
+      metaTags:[
+        {
+          name: 'keywords',
+          content: 'portfolio'
+        }
+      ]
+    }
   },
 ];
 
